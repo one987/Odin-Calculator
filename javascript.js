965483d0234b780fc747c5276e38456c08c4b1a6
one987 <joelbed@gmail.com>
2022-10-30 10:31:12 -0400

    basic operator function that takes two numbers and an operator then calls the right function

--- javascript.js
+++ javascript.js
@@ -2,16 +2,29 @@
 
 function add(x,y) {
     return x + y;
-}
+};
 
 function subtract(x,y) {
     return x - y;
-}
+};
 
 function multiply(x,y) {
     return x * y;
-}
+};
 
 function divide(x,y) {
     return x / y;
-}
+};
+
+function operate(x,y,operator) {
+    switch (operator) {
+        case "+":
+          return add(x,y);
+        case "-":
+          return subtract(x,y);
+        case "*":
+          return multiply(x,y);
+        case "/":
+          return divide(x,y);
+      }
+};
