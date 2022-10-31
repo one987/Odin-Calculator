dde16086b914585988dc4f35d41d2960c3e11b71
one987 <joelbed@gmail.com>
2022-10-31 00:25:28 -0400

    small display works, displays result and you can continue equation

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ let displayValue = '';
 let firstInt = null;
 let secondInt = null;
 let operator = '';
+let result = '';
 
 function concat(firstInt, secondInt) {
     int = '' + firstInt + secondInt;
@@ -18,6 +19,7 @@ const delKey = document.querySelector('.delete');
 const equalsKey = document.querySelector('.equals');
 const pointKey = document.querySelector('.point');
 const display = document.getElementById('display');
+const smallDis = document.getElementById('small');
 
 //event listeners:
 key.forEach((button) =>
@@ -32,41 +34,58 @@ numKey.forEach((button) =>
             console.log(target.innerText); //log target
             displayValue = target.innerText; //update display value
             display.innerHTML = displayValue;//update display html
-            firstInt = displayValue;
-            
+            smallDis.innerText = displayValue;
         } else if (displayValue !== '') {
             displayValue += target.innerText;
             display.innerHTML = displayValue;
+            smallDis.innerText = displayValue;
         }
-
-
     })
+
 );
 opKey.forEach((button) =>
     button.addEventListener('click', ({ target }) => {
         console.log(target.innerText);
+        operator = target.innerText;
+        firstInt = displayValue;
+        smallDis.innerText += operator;
+        console.log(firstInt);
+        display.innerHTML = ''
+        displayValue = ''
+        // clear();
+        //add smaller font equation display
     })
-);
-clearKey.addEventListener('click', () => {
-    console.log('clear');
-    display.innerHTML = '';
-    displayValue = ''
-    console.log(`displayValue = ${displayValue}`);
 
-});
-delKey.addEventListener('click', () => {
-    console.log('delete');
-});
+);
+
 equalsKey.addEventListener('click', () => {
     console.log('=');
+    secondInt = displayValue;
+    result = operate(firstInt, secondInt, operator);
+    display.innerHTML = result;
+    smallDis.innerText = `${firstInt} ${operator} ${secondInt} = ${result}`;
+    displayValue = result;
+    
 });
+
+clearKey.addEventListener('click', () => {
+   clear();
+});
+
+delKey.addEventListener('click', () => {
+    console.log('delete');
+    display.innerHTML = displayValue.slice(0, -1);
+    
+
+});
+
 pointKey.addEventListener('click', () => {
     console.log('.');
 });
 
 //operator functions:
 function add(x, y) {
-    return x + y;
+    return Number(x) + Number(y);
 };
 
 function subtract(x, y) {
@@ -93,4 +112,12 @@ function operate(x, y, operator) {
             return divide(x, y);
     }
 };
+
+function clear() {
+    console.log('clear');
+    display.innerHTML = '';
+    displayValue = '';
+    smallDis.innerText = '';
+    console.log(`displayValue = ${displayValue}`);
+}
 
