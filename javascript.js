3ee4c6cf465b48470a0583e6d46c58433300245a
one987 <joelbed@gmail.com>
2022-11-02 14:49:32 -0400

    removed semicolons

--- javascript.js
+++ javascript.js
@@ -1,45 +1,45 @@
 //variables:
-let firstInt = '';
-let secondInt = '';
-let operator = '';
-let result = '';
-let isOpPressed = false;
-let isEqualPressed = false;
-let bruh = false;
+let firstInt = ''
+let secondInt = ''
+let operator = ''
+let result = ''
+let isOpPressed = false
+let isEqualPressed = false
+let bruh = false
 
 //query selectors:
-const key = document.querySelectorAll('.btn');
-const numKey = document.querySelectorAll('.num');
-const opKey = document.querySelectorAll('.op');
-const clearKey = document.querySelector('.clear');
-const delKey = document.querySelector('.delete');
-const equalsKey = document.querySelector('.equals');
-const pointKey = document.querySelector('.point');
-const display = document.getElementById('display');
-const smallDis = document.getElementById('small');
+const key = document.querySelectorAll('.btn')
+const numKey = document.querySelectorAll('.num')
+const opKey = document.querySelectorAll('.op')
+const clearKey = document.querySelector('.clear')
+const delKey = document.querySelector('.delete')
+const equalsKey = document.querySelector('.equals')
+const pointKey = document.querySelector('.point')
+const display = document.getElementById('display')
+const smallDis = document.getElementById('small')
 
 //event listeners:
 
 //all keys
 key.forEach((button) =>
     button.addEventListener('click', ({ target }) => {
-        console.log({ target });
+        console.log({ target })
     })
-);
+)
 
 //number keys
 numKey.forEach((button) =>
     button.addEventListener('click', ({ target }) => {
 
         if (isEqualPressed) {
-            clear();
-            updateDisplay(target);
+            clear()
+            updateDisplay(target)
         } else if (isOpPressed) {
-            clearDis();
-            isOpPressed = false;
-            updateDisplay(target);
+            clearDis()
+            isOpPressed = false
+            updateDisplay(target)
         } else if (display.innerText == '' || display.innerText !== '') {
-            updateDisplay(target);
+            updateDisplay(target)
         } 
     })
 );
@@ -47,139 +47,139 @@ numKey.forEach((button) =>
 //operator keys
 opKey.forEach((button) =>
     button.addEventListener('click', ({ target }) => {
-        isEqualPressed = false;
+        isEqualPressed = false
         if (display.innerText == '') {
             return
         } else if (firstInt && operator && bruh == false) {
-            firstInt = operate(firstInt, display.innerText, operator);
-            display.innerText = firstInt;
-            smallDis.innerText += operator;
-            operator = target.innerText;
-            isOpPressed = true;
-            divideByZero();
+            firstInt = operate(firstInt, display.innerText, operator)
+            display.innerText = firstInt
+            smallDis.innerText += operator
+            operator = target.innerText
+            isOpPressed = true
+            divideByZero()
         } else if (bruh) {
-            operator = target.innerText;
-            firstInt = display.innerText;
-            console.log(firstInt, operator);
-            smallDis.innerText += operator;
+            operator = target.innerText
+            firstInt = display.innerText
+            console.log(firstInt, operator)
+            smallDis.innerText += operator
             display.innerText = ''
-            bruh = false;
+            bruh = false
         } else {
-            operator = target.innerText;
-            firstInt = display.innerText;
-            console.log(firstInt, operator);
-            smallDis.innerText += operator;
+            operator = target.innerText
+            firstInt = display.innerText
+            console.log(firstInt, operator)
+            smallDis.innerText += operator
             display.innerText = ''
         }
     })
 
-);
+)
 
 // equals key
 equalsKey.addEventListener('click', () => {
     if (display.innerText == '' || firstInt == '') {
         return
     } else {
-        equals();
+        equals()
     }
-});
+})
 
 clearKey.addEventListener('click', () => {
-    clear();
-});
+    clear()
+})
 
 delKey.addEventListener('click', () => {
     if (isEqualPressed) {
-        clear();
+        clear()
     } else {
-        console.log('delete');
-        let str = display.innerText;
-        let str2 = smallDis.innerText;
-        display.innerText = str.slice(0, -1);
-        smallDis.innerText = str2.slice(0, -1);
+        console.log('delete')
+        let str = display.innerText
+        let str2 = smallDis.innerText
+        display.innerText = str.slice(0, -1)
+        smallDis.innerText = str2.slice(0, -1)
     }
-});
+})
 
 pointKey.addEventListener('click', ({ target }) => {
     if (display.innerText.includes('.') !== true) {
-        console.log('.');
-        updateDisplay(target);
+        console.log('.')
+        updateDisplay(target)
     }
-});
+})
 
 //functions:
 function equals() {
-    secondInt = display.innerText;
-    result = operate(firstInt, secondInt, operator);
-    display.innerText = result;
-    smallDis.innerText = `${firstInt} ${operator} ${secondInt} = ${result}`;
+    secondInt = display.innerText
+    result = operate(firstInt, secondInt, operator)
+    display.innerText = result
+    smallDis.innerText = `${firstInt} ${operator} ${secondInt} = ${result}`
     console.log(`${firstInt} ${operator} ${secondInt} = ${result}`)
-    isEqualPressed = true;
-    isOpPressed = false;
-    bruh = true;
-    divideByZero();
+    isEqualPressed = true
+    isOpPressed = false
+    bruh = true
+    divideByZero()
 
-};
+}
 
 function clear() {
-    display.innerText = '';
-    smallDis.innerText = '';
-    firstInt = '';
-    secondInt = '';
-    operator = '';
-    isOpPressed = false;
-    isEqualPressed = false;
-    bruh = false;
-    console.log('clear');
+    display.innerText = ''
+    smallDis.innerText = ''
+    firstInt = ''
+    secondInt = ''
+    operator = ''
+    isOpPressed = false
+    isEqualPressed = false
+    bruh = false
+    console.log('clear')
 
-};
+}
 
 function divideByZero() {
     if (display.innerText == 'Infinity' || display.innerText == 'NaN') {
-        alert('Beyond the Infinite!');
-        clear();
+        alert('Beyond the Infinite!')
+        clear()
     }
-};
+}
 
 function updateDisplay(target) {
-    display.innerText += target.innerText;
-    smallDis.innerText += target.innerText;
+    display.innerText += target.innerText
+    smallDis.innerText += target.innerText
 }
 
 function clearDis() {
-    display.innerText = '';
-    console.log('clear dis');
-};
+    display.innerText = ''
+    console.log('clear dis')
+}
 
 //operator functions:
 function add(x, y) {
-    return Number(x) + Number(y);
-};
+    return Number(x) + Number(y)
+}
 
 function subtract(x, y) {
-    return x - y;
-};
+    return x - y
+}
 
 function multiply(x, y) {
-    return x * y;
-};
+    return x * y
+}
 
 function divide(x, y) {
-    return x / y;
-};
+    return x / y
+}
 
 function operate(x, y, operator) {
     switch (operator) {
         case "+":
-            return add(x, y);
+            return add(x, y)
         case "-":
-            return subtract(x, y);
+            return subtract(x, y)
         case "*":
-            return multiply(x, y);
+            return multiply(x, y)
         case "/":
-            return divide(x, y);
+            return divide(x, y)
     }
-};
+}
 
 
 //to do:
